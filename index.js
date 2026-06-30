7c1b62fe99436c7d91ef08fd86a8082943d9b038
Gela0566 <myrahgelase@gmail.com>
2026-07-01 00:13:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,1 @@
-module.exports = async (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
 
-    if (req.method === 'OPTIONS') return res.status(200).end();
-
-    if (req.method === 'POST') {
-        const { action, paymentId } = req.body;
-        if (action === 'approve') {
-            return res.status(200).json({ message: "Approved", paymentId: paymentId });
-        }
-        if (action === 'complete') {
-            return res.status(200).json({ message: "Completed" });
-        }
-    }
-
-    return res.status(200).json({ status: "Serveur actif" });
-};
